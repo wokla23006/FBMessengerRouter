206a24014b017200e986a9775832d5b45a94b42a
Bernardo Covas <bernardo.covas@hotmail.com>
2018-08-06 09:39:31 +0100

    implemented receiver in python. Nodejs seems to be having some trouble keeping bytes in propper order.

--- listen.py
+++ listen.py
@@ -1,4 +1,9 @@
 import fbchat
+import requests
+import threading
+import queue
+import os
+import wget
 
 email = "mbot.receiver@gmail.com"
 psswd = "sdasdfgdsfgaf34t937hnx9027y"
@@ -6,20 +11,57 @@ psswd = "sdasdfgdsfgaf34t937hnx9027y"
 email = "bernardo.covas@hotmail.com"
 psswd = "uoT5f8y4johsIJDECXMBHL7j"
 
-client = fbchat.Client(email, psswd)
 
-class Listener(fbchat.Client):
+class Downloader(fbchat.Client):
 
-    def onMessage(self, author_id, message_object: fbchat.Message, 
-                        thread_id, thread_type, **kwargs):
-                        
-        attatchments = message_object.attatchments
+    downloaded = []
 
-        for attatchment in attatchments:
+    def _cleanFirstRequest(self, dirty_request):
+        
+        i = dirty_request.find("document.location.replace")
+        dirty_request = dirty_request[i:]
+        i = dirty_request.find('");')
+        dirty_request = dirty_request[:i]
+        dirty_request = dirty_request.replace('document.location.replace("', "")
+        dirty_request = dirty_request.replace("\\", "")
+        return dirty_request
+
+    def onMessage(self, mid, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
+        print("Got message.")
+        
+        dirname = "./" + thread_id
+        assert isinstance(message_object, fbchat.models.Message )
+
+        if message_object.text == "done":
+            print("Done.")
+            with open(dirname + "/payload", "wb") as p:
+                self.downloaded.sort()
+                for download_filename in self.downloaded:
+                    with open(download_filename, "rb") as f:
+                        p.write(f.read())
+            return
+
+        if len(message_object.attachments) > 0:
+        
+            attch = message_object.attachments[0]
+            assert isinstance(attch, fbchat.models.FileAttachment)
+            filename = dirname + "/" + attch.name
+
+            html_new_request = requests.get(attch.url, allow_redirects=True).text
+            url = self._cleanFirstRequest(html_new_request)
+
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+
+            if (os.path.exists(filename)):
+                os.remove(filename)
+
+            wget.download(url, bar=None, out=filename)
+            self.downloaded.append(filename)
+
+            print(url)
             
-            print(message_object)        
         return
 
-
-client = Listener(email, psswd)
+client = Downloader(email, psswd)
 client.listen()
